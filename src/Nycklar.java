09833828e1a1048f9ac93f4cb067802c47fb0ee0
Omarmed3rochsen123 <leo.almeblad@elev.ga.ntig.se>
2023-09-06 11:53:54 +0200

    Initial commit

---
+++ src/Nycklar.java
@@ -0,0 +1,12 @@
+public class Nycklar {
+    int size;
+    String shape;
+    String color;
+
+    static void UnlockDoor(){
+        System.out.println("lås ");
+    }
+    static void PickUp(){
+        System.out.println("ta upp den lol");
+    }
+}
---
+++ src/Shoes.java
@@ -0,0 +1,17 @@
+public class Shoes {
+    String brand;
+    String color;
+    int size;
+    String laceColor;
+    double creasePercent;
+
+    static void takeOffShoes(){
+        System.out.println("Ta av ");
+    }
+    static void tieLaces(){
+
+    }
+    static void kickPlayer(){
+        System.out.println("idk");
+    }
+}
